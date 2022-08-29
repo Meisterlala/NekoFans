989e0bd113bc8640b83124b647b2a4070399b016
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2022-08-29 22:09:45 +0200

    Formatted Json

--- Neko/Sources/APIS/Twitter.cs
+++ Neko/Sources/APIS/Twitter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -47,7 +48,7 @@ public class Twitter : IImageSource
     public Twitter(string search)
     {
         query = search;
-        URL = $"{URLSearch}?{string.Join('&', URLSearchParams)}{Uri.EscapeDataString(search)}";
+        URL = $"{URLSearch}?{string.Join('&', URLSearchParams)}{Uri.EscapeDataString(search).Replace(" ", "%20")}";
 
         HttpRequestMessage request() =>
             new(HttpMethod.Get, URL)
@@ -64,21 +65,26 @@ public class Twitter : IImageSource
     public async Task<NekoImage> Next(CancellationToken ct = default)
     {
         var searchResult = await URLs.GetURL();
-        var image = await Common.DownloadImage(searchResult.Media.url, ct);
+        var image = await Common.DownloadImage(searchResult.Media.Url, ct);
         image.Description = TweetDescription(searchResult.Tweet, searchResult.Author);
         image.URLClick = URLTweetID(searchResult.Tweet, searchResult.Author);
         return image;
     }
 
 
+    public static Task<int> GetTweetCount()
+    {
+
+    }
+
     private static readonly Regex removeTco = new(@"(\W*https:\/\/t.co\/\w+)+\W*$", RegexOptions.Compiled);
 
     public static string TweetDescription(TwitterSearchJson.Tweet tweet, TwitterSearchJson.User user)
     {
         // Remove t.co links from the tweet
-        var filterdText = removeTco.Replace(tweet.text, "");
+        var filterdText = removeTco.Replace(tweet.Text ?? "", "");
         // Convert time to local time
-        var localTime = DateTime.Parse(tweet.created_at).ToLocalTime();
+        var localTime = tweet.CreatedAt.ToLocalTime();
         var span = DateTime.Now - localTime;
 
         var time = span.TotalSeconds < 5
@@ -93,19 +99,19 @@ public class Twitter : IImageSource
             ? $"{(int)span.TotalDays} days ago"
             : $"{localTime.Date.ToLongTimeString()}";
 
-        return $"{user.name} (@{user.username}) tweeted {time}:\n{filterdText}";
+        return $"{user.Name} (@{user.Username}) tweeted {time}:\n{filterdText}";
     }
 
     private static string URLTweetID(TwitterSearchJson.Tweet tweet, TwitterSearchJson.User user)
-        => $"https://twitter.com/{user.username}/status/{tweet.id}";
+        => $"https://twitter.com/{user.Username}/status/{tweet.Id}";
 
     public struct SearchResult
     {
         public TwitterSearchJson.Tweet Tweet;
         public TwitterSearchJson.User Author;
-        public TwitterSearchJson.Media Media;
+        public TwitterSearchJson.Medium Media;
 
-        public SearchResult(TwitterSearchJson.Tweet tweet, TwitterSearchJson.User author, TwitterSearchJson.Media media)
+        public SearchResult(TwitterSearchJson.Tweet tweet, TwitterSearchJson.User author, TwitterSearchJson.Medium media)
         {
             Tweet = tweet;
             Author = author;
@@ -113,39 +119,46 @@ public class Twitter : IImageSource
         }
     }
 
-#pragma warning disable
+    #region JSON
+#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+
     public class TwitterSearchJson : IJsonToList<SearchResult>
     {
         public List<SearchResult> ToList()
         {
-            if (includes == null || includes.media == null || data == null)
+            if (Includes == null || Includes.Media == null || Data == null)
                 return new List<SearchResult>();
 
             List<SearchResult> results = new();
 
+            // Check for metadata
+            if (Includes.Users == null)
+                return results;
+
+
             // For each tweet
-            foreach (var tweet in data)
+            foreach (var tweet in Data)
             {
                 // that has media attached
-                if (tweet.attachments == null)
+                if (tweet.Attachments == null)
                     continue;
 
                 // Only show sensitive tweets if NSFW mode
-                if (tweet.possibly_sensitive ?? false && !Neko.NSFW.AllowNSFW)
+                if (tweet.PossiblySensitive && !NSFW.AllowNSFW)
                     continue;
 
                 // Find the Author
-                var author = includes.users.Find((user) => user.id == tweet.author_id);
+                var author = Includes.Users.Find((user) => user.Id == tweet.AuthorId);
                 if (author == null) // Every tweet should have an author
                     continue;
 
                 // Search all attatched media_keys
-                foreach (var media_key in tweet.attachments.media_keys)
+                foreach (var media_key in tweet.Attachments.MediaKeys ?? new())
                 {
                     // Find the media with the matching media_key
-                    var media = includes.media.Find((m) => m.media_key == media_key);
+                    var media = Includes.Media.Find((m) => m.MediaKey == media_key);
                     // That is an image
-                    if (media == null || media.type != "photo")
+                    if (media == null || media.Type != "photo")
                         continue;
                     // Add the image to the results
                     results.Add(new SearchResult(tweet, author, media));
@@ -155,56 +168,121 @@ public class Twitter : IImageSource
             return results;
         }
 
-        public List<Tweet> data { get; set; }
-        public Includes? includes { get; set; }
-        public Meta meta { get; set; }
+        [JsonPropertyName("data")]
+        public List<Tweet>? Data { get; set; }
 
-        public class Meta
-        {
-            public string? newest_id { get; set; }
-            public string? oldest_id { get; set; }
-            public int result_count { get; set; }
-            public string? next_token { get; set; }
-        }
+        [JsonPropertyName("includes")]
+        public Include? Includes { get; set; }
 
-        public class User
-        {
-            public string id { get; set; }
-            public string name { get; set; }
-            public string username { get; set; }
-        }
-
-        public class Media
-        {
-            public string media_key { get; set; }
-            public string type { get; set; }
-            public string url { get; set; }
-        }
-
-        public class Includes
-        {
-            public List<Media> media { get; set; }
-            public List<User> users { get; set; }
-        }
+        [JsonPropertyName("meta")]
+        public Metadata Meta { get; set; }
 
         public class Attachments
         {
-            public List<string> media_keys { get; set; }
+            [JsonPropertyName("media_keys")]
+            public List<string>? MediaKeys { get; set; }
         }
 
         public class Tweet
         {
-            public Attachments attachments { get; set; }
+            [JsonPropertyName("attachments")]
+            public Attachments? Attachments { get; set; }
 
-            public string author_id { get; set; }
-            public string id { get; set; }
-            public string text { get; set; }
-            public string created_at { get; set; }
-            public bool? possibly_sensitive { get; set; }
+            [JsonPropertyName("author_id")]
+            public string AuthorId { get; set; }
+
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+
+            [JsonPropertyName("text")]
+            public string Text { get; set; }
+
+            [JsonPropertyName("created_at")]
+            public DateTime CreatedAt { get; set; }
+
+            [JsonPropertyName("possibly_sensitive")]
+            public bool PossiblySensitive { get; set; }
         }
 
+        public class Include
+        {
+            [JsonPropertyName("media")]
+            public List<Medium>? Media { get; set; }
+
+            [JsonPropertyName("users")]
+            public List<User> Users { get; set; }
+        }
+
+        public class Medium
+        {
+            [JsonPropertyName("media_key")]
+            public string MediaKey { get; set; }
+
+            [JsonPropertyName("type")]
+            public string Type { get; set; }
+
+            [JsonPropertyName("url")]
+            public string Url { get; set; }
+        }
+
+        public class Metadata
+        {
+            [JsonPropertyName("newest_id")]
+            public string NewestId { get; set; }
+
+            [JsonPropertyName("oldest_id")]
+            public string OldestId { get; set; }
+
+            [JsonPropertyName("result_count")]
+            public int ResultCount { get; set; }
+
+            [JsonPropertyName("next_token")]
+            public string? NextToken { get; set; }
+        }
+
+        public class User
+        {
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("username")]
+            public string Username { get; set; }
+        }
     }
-#pragma warning restore
+
+    public class CountJson
+    {
+        [JsonPropertyName("data")]
+        public List<Datum> Data { get; set; }
+
+        [JsonPropertyName("meta")]
+        public Metadata Meta { get; set; }
+
+        public class Metadata
+        {
+            [JsonPropertyName("total_tweet_count")]
+            public int TotalTweetCount { get; set; }
+        }
+
+        public class Datum
+        {
+            [JsonPropertyName("end")]
+            public DateTime End { get; set; }
+
+            [JsonPropertyName("start")]
+            public DateTime Start { get; set; }
+
+            [JsonPropertyName("tweet_count")]
+            public int TweetCount { get; set; }
+        }
+    }
+
+#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+    #endregion
+
 }
 
 internal class TwitterMultiURLs : MultiURLsGeneric<Twitter.TwitterSearchJson, Twitter.SearchResult>
@@ -220,7 +298,7 @@ internal class TwitterMultiURLs : MultiURLsGeneric<Twitter.TwitterSearchJson, Tw
     // retrieve next_token from json response
     protected override void OnTaskSuccessfull(Twitter.TwitterSearchJson result)
     {
-        next_token = result.meta.next_token;
+        next_token = result.Meta.NextToken;
         base.OnTaskSuccessfull(result);
     }
 
