93c2465c78295e95324b0390e2b356bde4a9060f
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2022-10-21 17:48:39 +0200

    Caching of URLs for nekos.best

--- Neko/Sources/APIS/NekosBest.cs
+++ Neko/Sources/APIS/NekosBest.cs
@@ -38,45 +38,54 @@ public class NekosBest : IImageSource
     public bool Faulted { get; set; }
 
     private readonly string categoryName;
-    private readonly string url;
+    private readonly MultiURLs<NekosBestJson> urls;
 
     public NekosBest(string categoryName)
     {
         this.categoryName = categoryName;
-        url = $"https://nekos.best/api/v2/{categoryName}";
+        urls = new($"https://nekos.best/api/v2/{categoryName}?amount=20", this, 15);
     }
 
     public async Task<NekoImage> Next(CancellationToken ct = default)
     {
-        var json = await Download.ParseJson<NekosBestJson>(url, ct);
-
-        return json.Results is null || json.Results.Count == 0
-            ? throw new Exception($"No results in response from {url}")
-            : await Download.DownloadImage(json.Results[0].Url, typeof(NekosBest), ct);
+        var url = await urls.GetURL(ct);
+        return await Download.DownloadImage(url, typeof(NekosBest), ct);
     }
 
-    public override string ToString() => $"nekos.best {categoryName}";
+    public override string ToString() => $"nekos.best {categoryName} {urls}";
 
     public string Name => "nekos.best";
 
     public bool Equals(IImageSource? other) => other != null && other is NekosBest nb && nb.categoryName == categoryName;
 
 #pragma warning disable
-    public class NekosBestJson
+    public class NekosBestJson : IJsonToList<string>
     {
         [JsonPropertyName("results")]
         public List<Result> Results { get; set; }
 
+        public List<string> ToList()
+        {
+            var list = new List<string>();
+
+            if (Results is null || Results.Count == 0)
+                throw new Exception($"No results in response from nekos.best");
+
+            foreach (var result in Results)
+                list.Add(result.Url);
+            return list;
+        }
+
         public class Result
         {
             [JsonPropertyName("artist_href")]
-            public string ArtistHref { get; set; }
+            public string? ArtistHref { get; set; }
 
             [JsonPropertyName("artist_name")]
-            public string ArtistName { get; set; }
+            public string? ArtistName { get; set; }
 
             [JsonPropertyName("source_url")]
-            public string SourceUrl { get; set; }
+            public string? SourceUrl { get; set; }
 
             [JsonPropertyName("url")]
             public string Url { get; set; }
