ef31feeee231b5675d9d33aef99e522d563c7969
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2023-01-12 14:57:39 +0100

    .Net7 NSFW

--- resources/generate-repo.ps1
+++ resources/generate-repo.ps1
@@ -8,7 +8,7 @@ $branch = "master"
 $nekoname = "NekoFans.zip"
 $nekolewdname = "NekoFansLewd.zip"
 
-$nekolewdtag = "1.0.1-NSFW"
+$nekolewdtag = "1.0.2-NSFW"
 
 # Authorization header for the Github API.
 $header = @{
