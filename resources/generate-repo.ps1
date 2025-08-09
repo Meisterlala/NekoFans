47ee8c62e37ef80f6f6d3721a3bc748418aad0a6
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-09 14:12:10 +0200

    update version in repo

--- resources/generate-repo.ps1
+++ resources/generate-repo.ps1
@@ -6,7 +6,7 @@ $repo = "NekoFans"
 $nekoname = "NekoFans.zip"
 $nekolewdname = "NekoFansLewd.zip"
 
-$nekolewdtag = "1.0.8-NSFW"
+$nekolewdtag = "1.0.9-NSFW"
 
 # Authorization header for the Github API.
 $header = @{
